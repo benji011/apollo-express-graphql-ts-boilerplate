5a77f0fa22f0ecfbe218689b9d230d73abf711dd
benji011 <wolfnun011@gmail.com>
2021-03-12 11:41:14 +0900

    fix: Added endpoint for root and graphql

--- src/index.ts
+++ src/index.ts
@@ -25,7 +25,9 @@ const main = async () => {
     })
   })
   app.listen(3000, () => {
-    console.log('server started on http://localhost:3000/')
+    console.log(
+      `server started on http://localhost:3000/ \nGraphQL server -> http://localhost:3000/graphql`
+    )
   })
 }
 
